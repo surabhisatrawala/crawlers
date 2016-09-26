2d17ce541a61f569e53a83787b4817a467d2bc25
surabhisatrawala <ssatrawala@gmail.com>
2016-09-26 12:42:49 +0530

    Update Gt_crawl_main_url.py

--- Gt_crawl_main_url.py
+++ Gt_crawl_main_url.py
@@ -6,18 +6,26 @@ import re
 import os
 import sys
 import ssl
+from Gt_crawl_each_cat import line_from_file
 
-url="https://www.getapp.com/browse"
-b=open("getapp.csv",'w')
-hdr = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3','Accept-Encoding': 'none','Accept-Language': 'en-US,en;q=0.8','Connection': 'keep-alive'}
-req = urllib2.Request(url,headers=hdr)
-htmltext = urllib2.urlopen(req).read()
-soup = BeautifulSoup(htmltext,"html.parser")
-for tag in soup.find_all('a',attrs={"style" : "font-family:sans-serif;font-weight:normal;letter-spacing:0"}):
-	data=tag['href']
-	print data
-	b.write("https://www.getapp.com"+data)
-	b.write("\n")
+def crawl_main_url():
+	b=open("getapp.csv",'w')
+ 	url="https://www.getapp.com/browse"
+	hdr = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3','Accept-Encoding': 'none','Accept-Language': 'en-US,en;q=0.8','Connection': 'keep-alive'}
+	req = urllib2.Request(url,headers=hdr)
+	htmltext = urllib2.urlopen(req).read()
+	soup = BeautifulSoup(htmltext,"html.parser")
+	for tag in soup.find_all('a',attrs={"style" : "font-family:sans-serif;font-weight:normal;letter-spacing:0"}):
+		data=tag['href']
+		print data
+		b.write("https://www.getapp.com"+data)
+		b.write("\n")
+		
+crawl_main_url()
+line_from_file('C:/Python27/nytimes/getapp1/getapp.csv') #calling line_from_file to crawl each category frpom getapp file
+
+
+
 
 
 
